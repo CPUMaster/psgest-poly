655f8c9a0b1f173cb154debd3485f7199544637b
CPU_Master <adil.smaili@gmail.com>
2017-08-24 18:18:58 +0100

    height solve

--- data/dataGen.js
+++ data/dataGen.js
@@ -3,33 +3,33 @@ var util = require('util')
 var JsonFaker = require('json-faker')
 
 var objectTemplate = {
-  'id': '@faker.random.uuid()',
-  'title': '@faker.name.findName()',
-  'description': '@faker.lorem.words(80)',
-  'image': '@faker.random.image(400)',
-  'random': '@faker.random.arrayElement([1,2,3,4,5,6,7,8,9,10])',
-  'date': '@faker.date.recent()'
+    'id': '@faker.random.uuid()',
+    'title': '@faker.name.findName()',
+    'description': '@faker.lorem.words(80)',
+    'image': '@faker.random.image(400)',
+    'random': '@faker.random.arrayElement([1,2,3,4,5,6,7,8,9,10])',
+    'date': '@faker.date.recent()'
 
 }
 
 var Obj = {
-  'articles': []
+    'articles': []
 }
 var articles = []
-var len = 100
+var len = 1000
 
-while(len--) {
-  var fakeJSON = JsonFaker.process(objectTemplate) // returns object
-  Obj.articles.push(fakeJSON)
+while (len--) {
+    var fakeJSON = JsonFaker.process(objectTemplate) // returns object
+    Obj.articles.push(fakeJSON)
 }
 console.log(fakeJSON)
-  // Obj.articles.push(articles)
+    // Obj.articles.push(articles)
 
 fs.writeFile('./db.json', JSON.stringify(Obj), function(err) {
 
-  if(err) {
-    return console.log(err)
-  }
+    if (err) {
+        return console.log(err)
+    }
 
-  console.log('The file was saved!')
+    console.log('The file was saved!')
 });
